b1e2f43e71e93a3613d064e5b01d0847c81d6578
Coding Kevin BKH <codingkevinbkh@gmail.com>
2020-05-23 22:13:01 -0600

    Remove Linked List Elements solution (#17)

---
+++ python/problems-0201-0300/0203-remove-linked-list-elements.py
@@ -0,0 +1,46 @@
+'''
+歡迎訂閱我們的Youtube頻道Coding Kevin BKH，影片中會使用動畫解釋演算法，同時也有Leetcode實戰解題。
+Check out our Youtube channel, we explain the solutions step by step. Please subscribe!
+The audio is in Mandarin
+
+Coding Kevin BKH
+
+'''
+
+### solution 1 ###
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode()
+        prev = dummy
+        curr = head
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev.next = curr
+                prev = curr
+            curr = curr.next
+        return dummy.next
+
+### solution 2 ###
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode()
+        dummy.next = head
+        curr = dummy
+        while curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return dummy.next
