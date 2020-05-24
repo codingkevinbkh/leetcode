bafbba6141ee0a3a1e385578ae0ff63d4b1dfd7a
Coding Kevin BKH <codingkevinbkh@gmail.com>
2020-05-24 12:24:40 -0600

    N-ary Tree Preorder Traversal solution (#21)

---
+++ python/problems-0501-0600/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,31 @@
+'''
+歡迎訂閱我們的Youtube頻道Coding Kevin BKH，影片中會使用動畫解釋演算法，同時也有Leetcode實戰解題。
+Check out our Youtube channel, we explain the solutions step by step. Please subscribe!
+The audio is in Mandarin
+
+Coding Kevin BKH
+
+'''
+
+### solution 1 ###
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        def traverse(head):
+            nodes.append(head.val)
+            for node in head.children:
+                traverse(node)
+
+        if root:
+            nodes = []
+            traverse(root)
+            return nodes
+        else:
+            return None
