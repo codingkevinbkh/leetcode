58bc9efae3489019848ee71f0ceb340edfefa4c8
Coding Kevin BKH <codingkevinbkh@gmail.com>
2020-05-28 00:59:15 -0600

    Invert Binary Tree solution (#34)

---
+++ python/problems-0201-0300/0226-invert-binary-tree.py
@@ -0,0 +1,28 @@
+'''
+歡迎訂閱我們的Youtube頻道Coding Kevin BKH，影片中會使用動畫解釋演算法，同時也有Leetcode實戰解題。
+Check out our Youtube channel, we explain the solutions step by step. Please subscribe!
+The audio is in Mandarin
+
+Coding Kevin BKH
+
+'''
+
+### solution 1 ###
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        def invert(head):
+            if head.left:
+                invert(head.left)
+            if head.right:
+                invert(head.right)
+            head.left, head.right = head.right, head.left
+        
+        if root:
+            invert(root)
+        return root
