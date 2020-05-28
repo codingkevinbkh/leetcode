29f499f9299762d52bcbbc3e2f75de0bfee2020e
Coding Kevin BKH <codingkevinbkh@gmail.com>
2020-05-28 17:59:05 -0600

    Binary Search Tree To Greater Sum Tree solution (#36)

---
+++ python/problems-1001-1100/1038-binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,31 @@
+'''
+歡迎訂閱我們的Youtube頻道Coding Kevin BKH，影片中會使用動畫解釋演算法，同時也有Leetcode實戰解題。
+Check out our Youtube channel, we explain the solutions step by step. Please subscribe!
+The audio is in Mandarin
+
+Coding Kevin BKH
+
+'''
+
+### solution 1 ###
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        def convert(head, parent_max):
+            if head.right:
+                head.val += convert(head.right, parent_max)
+            else:
+                head.val += parent_max
+            if head.left:
+                return convert(head.left, head.val)
+            else:
+                return head.val
+                
+        if root:
+            convert(root, 0)
+        return root
