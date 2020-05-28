00e2ff02efe6bcc125396ee76d5e88bd7b5dbf76
Coding Kevin BKH <codingkevinbkh@gmail.com>
2020-05-28 00:06:54 -0600

    Binary Search solution (#33)

---
+++ python/problems-0701-0800/0704-binary-search.py
@@ -0,0 +1,22 @@
+'''
+歡迎訂閱我們的Youtube頻道Coding Kevin BKH，影片中會使用動畫解釋演算法，同時也有Leetcode實戰解題。
+Check out our Youtube channel, we explain the solutions step by step. Please subscribe!
+The audio is in Mandarin
+
+Coding Kevin BKH
+
+'''
+
+### solution 1 ###
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid+1
+            else:
+                right = mid-1
+        return -1
