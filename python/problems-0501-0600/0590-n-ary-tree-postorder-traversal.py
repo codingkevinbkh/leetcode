15b2dcc450a5f25d0ee283fceff78191a65a4871
Coding Kevin BKH <codingkevinbkh@gmail.com>
2020-05-27 21:39:13 -0600

    N-ary Tree Postorder Traversal solution (#28)

---
+++ python/problems-0501-0600/0590-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,29 @@
+'''
+歡迎訂閱我們的Youtube頻道Coding Kevin BKH，影片中會使用動畫解釋演算法，同時也有Leetcode實戰解題。
+Check out our Youtube channel, we explain the solutions step by step. Please subscribe!
+The audio is in Mandarin
+
+Coding Kevin BKH
+
+'''
+
+### solution 1 ###
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        def traverse(head):
+            for child in head.children:
+                traverse(child)
+            nodes.append(head.val)
+        
+        nodes = []
+        if root:
+            traverse(root)
+        return nodes
